599abf794dd4779cf3563e33ae239c51abbf1830
ludo <ludo@LUDO>
2018-01-21 21:24:56 +0100

    Ajoutez des fichiers projet.

---
+++ DateTimeHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodesExtension
+{
+   
+    
+    public static class DateTimeHelper
+    {
+       
+
+        public static bool IsWeekEnd(this DateTime dt)
+        {
+            if ((dt.DayOfWeek == DayOfWeek.Saturday) || (dt.DayOfWeek == DayOfWeek.Sunday))              
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static DateTime GetLastDayOfMonth(this DateTime dt)
+        {
+            int daysInMonth = DateTime.DaysInMonth(dt.Year, dt.Month);
+
+            return new DateTime(dt.Year,dt.Month,daysInMonth);
+               
+           
+        
+        }
+
+        public static bool IsInRange(DateTime dt, DateTime debut, DateTime fin)
+        {
+            if (dt >= debut && dt <= fin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodesExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date;
+            date = DateTime.Now;
+
+            bool test = DateTimeHelper.IsWeekEnd(date);
+
+            Console.WriteLine(test);
+
+
+            date = DateTimeHelper.GetLastDayOfMonth(date);
+
+            Console.WriteLine(date);
+
+            Console.ReadKey();
+        }
+    }
+}
